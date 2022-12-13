1b15140ea07cb8d06aa0bed861aa36a4d427093d
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-12-13 17:56:21 +0545

    TodoList component created

---
+++ src/features/TodosList.js
@@ -0,0 +1,13 @@
+//react query imports
+import { useQuery, useMutaion, useQueryClient } from "react-query";
+
+import { getTodos, updateTodos, deleteTodos, addTodos } from "../api/todosApi";
+
+import { FontAwesomeIcon } from "@fontawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fontawesome/free-solid-svg-icons";
+
+const TodosList = () => {
+  return <div>TodosList</div>;
+};
+
+export default TodosList;
